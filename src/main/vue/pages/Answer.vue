12d9ecbb1ea6f54aa9dcad1d00395bc410856314
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-05-10 12:45:20 +0200

    added answer and survey page for single surveys and added to router  refs #14188@0.75h

---
+++ src/main/vue/pages/Answer.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Survey ausfüllen</h1>
+        <div>
+            Titel: <input type="text" v-model="survey.title">
+        </div>
+        <div>
+            Text: <textarea v-model="survey.text" rows="7"></textarea>
+        </div>
+        <button @click="save">Speichern</button>
+    </div>
+</template>
+
+<script>
+    import {mapGetters, mapMutations} from "vuex";
+
+    export default {
+        name: "Answer",
+        data() {
+            return {
+                id: 0,
+                article: {}
+            }
+        },
+        created() {
+            this.id = this.$route.params.id
+            this.survey = this.getSurvey(this.id)
+        },
+        computed: {
+            ...mapGetters(['getSurvey']),
+        },
+        methods: {
+            ...mapMutations([
+                'updateSurvey'
+            ]),
+            answer() {
+                this.updateSurvey(this.survey)
+                this.$router.push('/')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .my-row {
+        padding: 20px;
+    }
+</style>
---
+++ src/main/vue/pages/Survey.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>Dies ist die Surveyseite für die Survey mit der ID {{ id }}</p>
+    </div>
+
+</template>
+
+<script>
+
+    import {mapGetters} from "vuex";
+
+    export default {
+        name: "Survey",
+        data() {
+            return {
+                id: 0
+            }
+        },
+        created() {
+            this.id = this.$route.params.id
+        },
+        computed: {
+            ...mapGetters(['getSurvey']),
+            survey() {
+                return this.getSurvey(this.id)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .my-row {
+        padding: 20px;
+    }
+</style>
