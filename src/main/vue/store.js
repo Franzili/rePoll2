8340efae48b9c3eb29535a84d2ab3c4c09ed70d0
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-05-10 13:45:22 +0200

    added store.js with two dummy surveys for testing  refs #14188 @0.1h

---
+++ src/main/vue/store.js
@@ -0,0 +1,94 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        authenticated: null, //
+        surveys: [
+            {
+                id: 1,
+                title: "is there a survey ?",
+                items: [
+                    {
+                        id: 1,
+                        type: "checkbox",
+                        question: "am I working",
+                        possibilities: [
+                            {
+                                id: 1,
+                                text: "yes",
+                            },
+                            {
+                                id: 2,
+                                text: "no"
+                            }
+                        ]
+                    },
+                    {
+                        id: 2,
+                        type: "checkbox",
+                        question: "need more questions",
+                        possibilities: [
+                            {
+                                id: 1,
+                                text: "yes",
+                            },
+                            {
+                                id: 2,
+                                text: "no"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                id: 3,
+                title: "are there two ?",
+                items: [
+                    {
+                        id: 1,
+                        type: "checkbox",
+                        question: "am I working",
+                        possibilities: [
+                            {
+                                id: 1,
+                                text: "yes",
+                            },
+                            {
+                                id: 2,
+                                text: "no"
+                            }
+                        ]
+                    },
+                    {
+                        id: 2,
+                        type: "checkbox",
+                        question: "need more questions",
+                        possibilities: [
+                            {
+                                id: 1,
+                                text: "yes",
+                            },
+                            {
+                                id: 2,
+                                text: "no"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+    },
+    getters: {
+        getSurvey: (state) => {
+            return (id) => {
+                return state.surveys.find(survey => survey.id == id)
+            }
+        }/*,
+        isAuthenticated: (state) => {
+            return state.authenticated
+        }*/
+    }
+})
