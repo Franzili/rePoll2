8ea8b080703f8a0f8a78910c3ee2ee2cc6259966
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-05-10 12:45:20 +0200

    added answer and survey page for single surveys and added to router  refs #13734 @0.75h

--- src/main/vue/router.js
+++ src/main/vue/router.js
@@ -3,6 +3,8 @@ import CreateSurvey from "./pages/CreateSurvey";
 import Start from "./pages/Start";
 import Account from "./pages/Account";
 import SurveyList from "./pages/SurveyList";
+import Survey from "./pages/Survey";
+import Answer from "./pages/Answer";
 
 export default new VueRouter({
     mode: 'history',
@@ -22,6 +24,17 @@ export default new VueRouter({
         {
             path: '/surveys/',
             component: SurveyList
+        },
+        {
+            path: '/survey/:id(\\d+)',
+            component: Survey
+        },
+        {
+            path: '/survey/:id/answer',
+            component: Answer,
+            //meta: {
+            //    requiresAuth: true
+            //}
         }
     ]
 })
