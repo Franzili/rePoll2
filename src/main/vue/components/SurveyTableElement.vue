3fc264af2327c7c3b2914b44901c401775056c3a
Maximilian Martini <mmartini@techfak.uni-bielefeld.de>
2020-05-13 13:47:27 +0200

    Added adjustments for mobile version, lets spin #14293 @0.5h

--- src/main/vue/components/SurveyTableElement.vue
+++ src/main/vue/components/SurveyTableElement.vue
@@ -1,33 +1,69 @@
 <template>
-    <div class="my-ste">
-        <b-container>
-            <b-row style="text-align: center" cols="5">
-                <b-col>
-                    <p class="my-name">{{survey.name}}</p>
-                </b-col>
-            </b-row>
-            <b-row></b-row>
-            <b-row style="text-align: center;" cols="5">
-                <b-col>
-                    <p class="my-status">{{survey.status}}</p>
-                </b-col>
-                <b-col></b-col>
-                <b-col></b-col>
-                <b-col></b-col>
-                <b-col>
-                    <router-link class="my-config-link" :to="'/config/'">Konfiguration</router-link>
-                </b-col>
+    <div>
+        <!-- better for mobile version -->
+        <div v-if="isMobile()" class="my-ste">
+            <b-container>
+                <b-row style="text-align: center">
+                    <b-col>
+                        <p class="my-name">{{survey.name}}</p>
+                    </b-col>
+                </b-row>
 
-            </b-row>
-        </b-container>
+                <b-row>
+                    <b-col>
+                        <p class="my-status">{{survey.status}}</p>
+                    </b-col>
+                    <b-col>
+                        <p>
+                            <b-icon icon="gear" animation="spin" font-scale="2" @click="toConfig"/>
+                        </p>
+                    </b-col>
+
+                </b-row>
+                <b-row></b-row>
+            </b-container>
+        </div>
+
+        <!-- better for Desktop version -->
+        <div v-else class="my-ste">
+            <b-container>
+                <b-row style="text-align: center" cols="5">
+                    <b-col>
+                        <p class="my-name">{{survey.name}}</p>
+                    </b-col>
+                </b-row>
+                <b-row></b-row>
+                <b-row style="text-align: center;" cols="5">
+                    <b-col>
+                        <p class="my-status">{{survey.status}}</p>
+                    </b-col>
+                    <b-col></b-col>
+                    <b-col></b-col>
+                    <b-col></b-col>
+                    <b-col>
+                        <router-link class="my-config-link" :to="'/config/'">Konfiguration</router-link>
+                    </b-col>
+
+                </b-row>
+            </b-container>
+        </div>
     </div>
+
 
 </template>
 
 <script>
     export default {
         name: "SurveyListElement",
-        props: ["survey",]
+        props: ["survey"],
+        methods: {
+            isMobile() {
+                return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+            },
+            toConfig() {
+                return this.$router.push('/config/')
+            }
+        }
 
     }
 </script>
@@ -35,7 +71,7 @@
 <style scoped>
 
     .my-config-link {
-        font-size: 14px;
+        font-size: 18px;
         color: #7F7E7F;
     }
 
@@ -53,5 +89,7 @@
         background-color: white;
         font-family: Arial,sans-serif;
     }
+
+
 
 </style>
