4dcfa6b28f82c76ff7ff34f27fb2ae8b32e8b20b
Maximilian Martini <mmartini@techfak.uni-bielefeld.de>
2020-05-14 09:59:47 +0200

    first Setup page adjustments refs #14309 @0.2h

--- src/main/vue/pages/SurveySetup.vue
+++ src/main/vue/pages/SurveySetup.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="my-back">
         <nav-bar></nav-bar>
-        <HelloWorld style="text-align:center;" class="ml-auto" msg="Konfigurationsseite"/>
+        <div>
+            <b-container class="my-container">
+                <b-row style="text-align: center" class="my-row">
+                    <b-col>
+                        <HelloWorld style="text-align:center;" class="ml-auto" msg="Konfigurationsseite"/>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
     </div>
 </template>
 
@@ -22,5 +30,12 @@
     .my-back {
         min-height: 100vh;
         background-color: lightgray
+    }
+
+    .my-container {
+        alignment: center;
+        background-color: white;
+        margin-top: 30px;
+        height: 70vh;
     }
 </style>
