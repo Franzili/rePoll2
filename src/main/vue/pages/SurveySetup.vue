049b6e7a15523ab2bfbdf1e3479457b498ee2c81
Maximilian Martini <mmartini@techfak.uni-bielefeld.de>
2020-05-15 09:31:51 +0200

    Added components to setup page, radio-buttons v1 added #14309 @1.0h

--- src/main/vue/pages/SurveySetup.vue
+++ src/main/vue/pages/SurveySetup.vue
@@ -1,12 +1,49 @@
 <template>
     <div class="my-back">
         <nav-bar></nav-bar>
-        <div>
+        <div class="my-sh">
             <b-container class="my-container">
                 <b-row style="text-align: center" class="my-row">
                     <b-col>
                         <HelloWorld style="text-align:center;" class="ml-auto" msg="Konfigurationsseite"/>
                     </b-col>
+                </b-row>
+
+                <b-row align-h="center">
+                    <b-form>
+                        <b-button class="my-button">Bearbeiten</b-button>
+                        <b-button class="my-button">Analyse</b-button>
+                    </b-form>
+                </b-row>
+
+                <b-row>
+                    <p style="margin-left: 20vw; margin-top: 2vh">Design</p>
+                </b-row>
+
+                <b-row align-h="center">
+                    <b-col>
+                        <p>Status</p>
+
+                    </b-col>
+                    <b-col>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="iRadio1" value="option1" checked>
+                            <label class="form-check-label" for="iRadio1">In Bearbeitung</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="iRadio2" value="option2">
+                            <label class="form-check-label" for="iRadio2">Bereit</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="iRadio3" value="option3">
+                            <label class="form-check-label" for="iRadio3">Aktiviert</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="iRadio4" value="option4">
+                            <label class="form-check-label" for="iRadio4">Deaktiviert</label>
+                        </div>
+                    </b-col>
+
                 </b-row>
             </b-container>
         </div>
@@ -33,9 +70,18 @@
     }
 
     .my-container {
+        text-align: center;
+        box-shadow: 10px 10px 5px grey;
         alignment: center;
-        background-color: white;
+        background-color: #EEEDEE;
         margin-top: 30px;
         height: 70vh;
+    }
+    .my-button {
+        background-color: black;
+        font-size: 100%;
+        color: #ACABAB;
+        margin-left: 2vw;
+        margin-right: 2vw;
     }
 </style>
