b54945b52e33311d2a5221502cd8ac2a3ef696b5
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-05-16 12:33:21 +0200

    Surveyelemente zum aufüllen in Answer werden angezeigt #14188 @0.9

--- src/main/vue/pages/Answer.vue
+++ src/main/vue/pages/Answer.vue
@@ -1,6 +1,7 @@
 <!--
 surveyTabelElement als template testen
 -->
+<!--
 <template>
     <div style="text-align:center;">
         <nav-bar></nav-bar>
@@ -11,22 +12,22 @@ surveyTabelElement als template testen
         <div>
             Titel: <input type="text" v-model="survey.name">
         </div>
-        <!--
+        <!-
         items , also fragebn hier statt test
         <SurveyTableList v-bind:surveys="surveys"/>
 
          <SurveyItem v-bind:item="item" v-bind:edit="edit"/>
-        -->
+        ->
 
         <div :key="item.id" v-for="item in items">
-            <SurveyItem v-bind:item="item" v-bind:edit="edit"/>
+            <SurveyItem v-bind:item="item" v-bind:edit="false"/>
         </div>
 
-        <!--
+        <!-
         <div>
             <SurveyItem v-bind:items="items" v-bind:edit="edit"/>
         </div>
-        -->
+        ->
 
 
         <b-button class="my-button" variant="primary">Save</b-button>
@@ -34,12 +35,52 @@ surveyTabelElement als template testen
 
     </div>
 </template>
+-->
+
+<template>
+    <b-container class="mt-3">
+        <nav-bar></nav-bar>
+        <HelloWorld></HelloWorld>
+        <b-row>
+            <b-col>
+                <!--
+                <b-card header="Fill out your survey">
+                    -->
+                    <b-form @submit.prevent="save">
+                        <b-form-group>
+                            <!--
+                            <b-form-input type="text" v-model="survey.name"></b-form-input>
+                            -->
+                            <p class="my-name">{{survey.name}}</p>
+                        </b-form-group>
+                        <b-form-group>
+                            <!--
+                            <b-form-textarea type="textarea" rows="10" v-model="survey.item"></b-form-textarea>
+                            -->
+                            <div :key="item.id" v-for="item in survey.items">
+                                <SurveyItem v-bind:item="item" v-bind:edit="edit"/>
+                            </div>
+                        </b-form-group>
+                        <!--
+                        template save buttons!!!
+                        <b-button variant="primary" type="submit">Speichern</b-button>
+                        -->
+                        <b-button class="my-button" variant="primary">Save</b-button>
+                        <b-button class="my-button" variant="success">Submit!</b-button>
+                    </b-form>
+                <!--
+                </b-card>
+                -->
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
 
 
 <script>
     import NavBar from "../components/NavBar";
     import HelloWorld from "../components/HelloWorld";
-    import {mapGetters} from "vuex";
+    import {mapGetters, mapMutations} from "vuex";
     import SurveyItem from "../components/SurveyItem"; //mapMutations
 
     export default {
@@ -48,8 +89,16 @@ surveyTabelElement als template testen
         data() {
             return {
                 id: 0,
-                article: {}
+                survey: {}
+                /*
+                ,
+                items: []
+                */
             }
+        },
+        mounted() {
+            this.id = this.$route.params.id
+            this.survey = this.getSurvey(this.id)
         },
         created() {
             this.id = this.$route.params.id
@@ -66,14 +115,14 @@ surveyTabelElement als template testen
     methods: {
         isMobile() {
             return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-        }
-        /*...mapMutations([
+        },
+        ...mapMutations([
             'updateSurvey'
         ]),
         answer() {
             this.updateSurvey(this.survey)
             this.$router.push('/')
-        }*/
+        }
     },
     comments: {
         NavBar,
