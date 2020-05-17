bb0502ece372d18c6333bd2190f24f7f89331b58
Lukas Middelberg <lukas@montemedio.de>
2020-05-18 00:16:57 +0200

    Cleaned up login page refs #14619

--- src/main/vue/pages/Start.vue
+++ src/main/vue/pages/Start.vue
@@ -1,70 +1,39 @@
 <template>
     <div>
-        <!-- :style="{ backgroundColor:'lightblue'}" -->
         <nav-bar></nav-bar>
         <HelloWorld style="text-align:center;" class="ml-auto" msg="Welcome to the best Survey App!"/>
 
-        <!-- this is better for mobile version -->
-        <div v-if="isMobile()">
-            <b-form class="my-form">
-                <b-form-group
-                    label="Username:"
-                    description="We'll never share your username with anyone else.">
-                    <b-form-input
-                        required
-                        placeholder="Enter username"
-                    ></b-form-input>
-                </b-form-group>
-
-                <b-form-group label="Password:" label-for="input-2">
-                    <b-form-input
-                        type="password"
-                        required
-                        placeholder="Enter password"
-                    ></b-form-input>
-                </b-form-group>
-
-                <div>
-                    <b-button class="my-button" variant="success">Login</b-button>
-                    <b-button class="my-button" variant="secondary">Sign up</b-button>
-                </div>
-
-            </b-form>
-        </div>
 
         <!-- this is better for desktop version -->
-        <div v-else>
-            <b-container>
-                <b-row align-h="center" class="login">
-                    <b-col></b-col>
-                    <b-col>
-                        <b-form>
-                            <b-form-group
-                                label="Username:">
-                                <b-form-input
-                                    required
-                                    placeholder="Enter username"
-                                    v-bind="auth.username"
-                                ></b-form-input>
-                            </b-form-group>
+        <b-row align-h="center" class="login" fluid="md">
+            <b-col md="4">
+                <b-card>
+                    <b-alert variant="danger" :show="authenticated === false">
+                        Username and Password do not match :( Please try again!
+                    </b-alert>
+                    <b-form @submit.prevent="login()">
+                        <b-form-group
+                            label="Username:">
+                            <b-form-input
+                                required
+                                placeholder="Enter username"
+                                v-model="auth.username"
+                            ></b-form-input>
+                        </b-form-group>
 
-                            <b-form-group label="Password:" label-for="input-2">
-                                <b-form-input
-                                    required
-                                    placeholder="Enter password"
-                                    v-bind="auth.password"
-                                ></b-form-input>
-                            </b-form-group>
+                        <b-form-group label="Password:" label-for="input-2">
+                            <b-form-input
+                                required
+                                placeholder="Enter password"
+                                v-model="auth.password"
+                            ></b-form-input>
+                        </b-form-group>
 
-                            <b-button variant="primary" v-on:click="login()">Login</b-button>
-                            <b-button variant="secondary">Sign up</b-button>
-                        </b-form>
-                    </b-col>
-                    <b-col></b-col>
-
-                </b-row>
-            </b-container>
-        </div>
+                        <b-button variant="primary" v-on:click="login()">Login</b-button>
+                    </b-form>
+                </b-card>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -72,7 +41,7 @@
     import HelloWorld from '../components/HelloWorld.vue';
     import NavBar from "../components/NavBar";
 
-    import {mapActions, mapGetters} from "vuex";
+    import {mapActions, mapState} from "vuex";
 
     export default {
         name: "Start",
@@ -85,13 +54,8 @@
                 auth: {
                     username: '',
                     password: ''
-                }
+                },
             }
-        },
-        computed: {
-            ...mapGetters({
-                authenticated: 'isAuthenticated'
-            })
         },
         methods: {
             isMobile() {
@@ -99,11 +63,20 @@
             },
             ...mapActions(['requestToken']),
             login() {
-                console.log("MOIN MOIN IHR LANDRATTEN!");
-                this.requestToken(this.auth).then(() => this.$router.push('/'))
-                console.log("Landratten2");
+                this.requestToken(this.auth)
+                    .then(() => {
+                        this.$router.push("/surveys");
+                    })
+                    .catch(() => {
+
+                    })
             }
         },
+        computed: {
+            ...mapState({
+                authenticated: 'authenticated'
+            })
+        }
     }
 </script>
 <style scoped>
