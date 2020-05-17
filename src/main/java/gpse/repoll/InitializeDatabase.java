6ad97447e0e999bb84639c4583ac9f89ed32b40f
Lukas Middelberg <lukas@montemedio.de>
2020-05-17 19:13:07 +0200

    Added Test User JamesBond

--- src/main/java/gpse/repoll/InitializeDatabase.java
+++ src/main/java/gpse/repoll/InitializeDatabase.java
@@ -1,5 +1,6 @@
 package gpse.repoll;
 
+import gpse.repoll.domain.exceptions.NotFoundException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,15 @@ import org.springframework.transaction.support.TransactionTemplate;
 public class InitializeDatabase implements InitializingBean {
 
     private PollService pollService;
+    private UserService userService;
     private TransactionTemplate transactionTemplate;
 
     @Autowired
     public InitializeDatabase(PollService pollService,
+                              UserService userService,
                               PlatformTransactionManager transactionManager) {
         this.pollService = pollService;
+        this.userService = userService;
         this.transactionTemplate = new TransactionTemplate(transactionManager);
     }
 
@@ -37,6 +41,20 @@ public class InitializeDatabase implements InitializingBean {
         transactionTemplate.execute(status -> {
             Poll poll = pollService.addPoll("Poll 1");
             pollService.addTextQuestion(poll.getId(), "Warum magst du Gummibaerchen?", 1234);
+            return null;
+        });
+
+        transactionTemplate.execute(status -> {
+            try {
+                userService.getUser("JamesBond");
+            } catch (NotFoundException e) {
+                final User user = userService.addUser(
+                    "JamesBond",
+                    // Passwort: GutenTag
+                    "{bcrypt}$2a$04$l7XuBX6cPlD2gFP6Qfiggur/j9Mea43E8ToPVpn8VpdXxq9KAa97i",
+                    "Bob", "jbond@mi6.com"
+                );
+            }
             return null;
         });
     }
