9981cedd6b19f1e2740549511e5e6d6e9fa4dab7
lbrockmann <lbrockmann@techfak.de>
2020-05-23 22:06:29 +0200

    constructor in class Anonymity refs #14837

--- src/main/java/gpse/repoll/domain/Anonymity.java
+++ src/main/java/gpse/repoll/domain/Anonymity.java
@@ -6,4 +6,6 @@ public class Anonymity {
     public static final String PARTIALLY_ANONYMOUS = "partiallyAnonymous";
     public static final String NON_ANONYMOUS = "nonAnonymous";
 
+    private Anonymity() { };
+
 }
