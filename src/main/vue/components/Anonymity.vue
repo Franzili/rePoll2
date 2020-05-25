4bf69bf2c8af428a7ab8966b33c4f8ce980913d2
lbrockmann <lbrockmann@techfak.de>
2020-05-25 18:53:43 +0200

    radiobuttons only while poll is IN_PROCESS, warningbox works, buttons are disabled when waiting for confirmation, refs #14834 @1.5h

--- src/main/vue/components/Anonymity.vue
+++ src/main/vue/components/Anonymity.vue
@@ -1,34 +1,34 @@
 <template>
     <!-- anonymity -->
     <b-row align-h="center"
-           :aria-disabled="poll.status === 'READY'
-                       || poll.status === 'ACTIVATED'
-                       || poll.status === 'DEACTIVATED'">
-
+           v-if="poll.status === 'IN_PROCESS'">
 
         <div align="left">
-            <div align="center">
-                <h3>Anonymity</h3>
+            <div align="left">
+                <h4>Anonymity</h4>
             </div>
             <b-form-group >
                 <b-form-radio-group
-                    v-on:change="changeAnonymityQuestion"
+                    v-on:input="changeAnonymityQuestion"
                     v-model="anonymityChecked"
                     stacked >
-                    <b-form-radio value="ANONYMOUS">anonymous<br>
+                    <b-form-radio value="ANONYMOUS" :disabled="waitingForConfirmation">
+                        anonymous<br>
                         <small class="text-muted">
                             The participants are unknown and there are no data
                             about them. You get one link for all participants.
                         </small>
                     </b-form-radio>
-                    <b-form-radio value="PARTIALLY_ANONYMOUS">partially anonymous<br>
+                    <b-form-radio value="PARTIALLY_ANONYMOUS" :disabled="waitingForConfirmation">
+                        partially anonymous<br>
                         <small class="text-muted">
                             Es sind nur minimal Daten (i.d.R. ein technischer Schlüssel) vom
                             Teilnehmer bekannt. Außer dem technischen Schlüssel werden keine
                             Teilnehmerdaten persistiert.
                         </small>
                     </b-form-radio>
-                    <b-form-radio value="NON_ANONYMOUS">non-anonymous<br>
+                    <b-form-radio value="NON_ANONYMOUS" :disabled="waitingForConfirmation">
+                        non-anonymous<br>
                         <small class="text-muted">
                             Der Teilnehmer ist bekannt. Es liegen Daten vor. Jeder Teilnehmer
                             erhält einen personalisierten Link.
@@ -44,6 +44,8 @@
                 <b-button @click="dontChangeAnonymity" class="my-button">no</b-button>
             </b-alert>
         </div>
+        <p>{{anonymityChecked}} <br> {{this.poll.anonymity}}</p>
+
     </b-row>
 
 </template>
@@ -56,18 +58,25 @@
         props: ["poll"],
         data() {
             return {
-                anonymityChecked: 'NON_ANONYMOUS',
-                sureToChangeAnonymity: false
+                anonymityChecked: this.poll.anonymity,
+                sureToChangeAnonymity: false,
+                waitingForConfirmation: false
             }
         },
         methods: {
             changeAnonymityQuestion: function () {
                 console.log("Hallo!")
-                this.sureToChangeAnonymity = true
+                if (this.poll.anonymity !== this.anonymityChecked) {
+                    this.sureToChangeAnonymity = true
+                    this.waitingForConfirmation = true
+                } else {
+                    this.sureToChangeAnonymity = false
+                }
             },
             changeAnonymity() {
                 this.sureToChangeAnonymity = false
                 this.poll.anonymity = this.anonymityChecked
+                this.waitingForConfirmation = false
                 let pollCmd = this.poll
                 console.log("change")
                 console.log(this.poll.id)
@@ -81,6 +90,7 @@
             dontChangeAnonymity() {
                 this.sureToChangeAnonymity = false
                 this.anonymityChecked = this.poll.anonymity
+                this.waitingForConfirmation = false
             }
         }
     }
