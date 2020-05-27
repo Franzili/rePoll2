8abaee07201482164236e42b89d9f7eb5e5c0e1c
Michael Peresada <mperesada@techfak.uni-bielefeld.de>
2020-05-27 12:29:13 +0200

    refs #14836 added a doughnut chart, fitted charts into cards @0.5h

---
+++ src/main/vue/components/charts/DoughnnutChart.vue
@@ -0,0 +1,31 @@
+<script>
+
+    import {Doughnut} from 'vue-chartjs';
+
+    export default {
+        extends: Doughnut,
+        props: {
+            label: {
+                type: Array
+            },
+            chartData: {
+                type: Array
+            },
+            options: {
+                type: Object
+            }
+        },
+        mounted() {
+            const myData = this.chartData;
+            this.renderChart({
+                    labels: this.label,
+                    datasets: myData},
+                this.options);
+        },
+        name: "DoughnutChart"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main/vue/pages/TestCharts.vue
+++ src/main/vue/pages/TestCharts.vue
@@ -1,27 +1,44 @@
 <template>
     <div>
-        <BarChart :chart-data="chartdata.datasets" :options="options" :label="chartdata.labels"></BarChart>
+        <b-card class="chart-card">
+            <BarChart :chart-data="barDatasets" :options="options" :label="barLabels"></BarChart>
+        </b-card>
+        <b-card class="chart-card">
+            <DoughnutChart :chart-data="doughnutDatasets" :options="options" :label="doughnutLabels"></DoughnutChart>
+        </b-card>
     </div>
 </template>
 
 <script>
-    import BarChart from "../components/BarChart";
+    import BarChart from "../components/charts/BarChart";
+    import DoughnutChart from "../components/charts/DoughnnutChart";
 
     export default {
         name: "TestCharts",
         data () {
             return {
-                chartdata: {
-                    //here could be the answer possibilities
-                    labels: ['sehr gut', 'gut', 'mittel', 'schlecht', 'sehr schlecht'],
-                    datasets: [
-                        {
-                            label: 'Wie gut findest du Repoll',
-                            backgroundColor: '#f87979',
-                            data: [80, 100, 50, 20, 0]
-                        }
-                    ]
-                },
+                //here could be the answer possibilities
+                barLabels: ['sehr gut', 'gut', 'mittel', 'schlecht', 'sehr schlecht'],
+                barDatasets: [
+                    {
+                        label: 'Wie gut findest du Repoll',
+                        backgroundColor: '#3eab37 ', //02a097
+                        data: [80, 100, 50, 20, 0]
+                    },
+                    {
+                        label: 'Wie gut findest du die anderen Umfragetools',
+                        backgroundColor: '#f87979',
+                        data: [0, 10, 20, 50, 100]
+                    }
+                ],
+                doughnutLabels: ['sehr gut', 'gut', 'mittel', 'schlecht', 'sehr schlecht'],
+                doughnutDatasets: [
+                    {
+                        label: 'Wie gut findest du Repoll',
+                        backgroundColor: ['#3eab37', '#02a097', '#FFED36', '#6546F8', '#70E1F8'],
+                        data: [80, 100, 50, 20, 0]
+                    }
+                ],
                 options: {
                     responsive: true,
                     maintainAspectRatio: false
@@ -29,11 +46,14 @@
             }
     },
         components: {
+            DoughnutChart,
             BarChart
         }
     }
 </script>
 
 <style scoped>
-
+    .chart-card {
+        margin: 150px 200px;
+    }
 </style>
