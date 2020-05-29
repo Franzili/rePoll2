d033af2e17d9255d8f7e219d94de6bb174e58c90
Maximilian Martini <mmartini@techfak.uni-bielefeld.de>
2020-05-29 14:53:51 +0200

    added some combobox variants refs #14862 @1.0h

--- src/main/vue/components/poll-tabs/stats/Questions.vue
+++ src/main/vue/components/poll-tabs/stats/Questions.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
         <p>
+            <b-form-select v-model="selected" :options="comboChoice"></b-form-select>
+        </p>
+
+        <p>
+            <label>Input with datalist</label>
+            <b-form-input list="input-list" id="input-with-list"></b-form-input>
+            <b-form-datalist id="input-list" :options="comboChoice" v-model="selected"></b-form-datalist>
+        </p>
+
+        <p>
+            <b-form-input list="my-list-id"></b-form-input>
+
+            <datalist id="my-list-id">
+                <option>Manual Option</option>
+                <option></option>
+            </datalist>
+        </p>
+        <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
             dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
             clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
@@ -36,8 +54,39 @@
 </template>
 
 <script>
+
+    import {mapGetters} from "vuex";
+
     export default {
-        name: "Questions"
+        name: "Questions",
+        data() {
+            return {
+                tmpID: 0,
+                selected: null,
+                comboChoice: [{value: null, text: 'Select your question'}],
+                comboChoice2: [],
+            }
+        },
+        computed: {
+            ...mapGetters(['getPoll']),
+            poll() {
+                return this.getPoll(this.tmpID)
+            }
+        },
+        created: function () {
+            this.tmpID = this.$route.params.tmpPollID;
+            this.fillComboChoice()
+        },
+        methods: {
+            fillComboChoice() {
+                for (var i = 0; i < this.poll.questions.length; i++) {
+                    let choice = {value: this.poll.questions[i].id, text: this.poll.questions[i].title}
+                    this.comboChoice = [... this.comboChoice, choice]
+                    let choice2 = this.poll.questions[i].title
+                    this.comboChoice2 = [... this.comboChoice2, choice2]
+                }
+            }
+        }
     }
 </script>
 
