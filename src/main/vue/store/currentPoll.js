8b6e6ba11aa8f7d09797017dc911612affd0c250
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-05-31 15:21:35 +0200

    Added set and update mutations in currentPoll.js store  refs #15078 @0.1h

--- src/main/vue/store/currentPoll.js
+++ src/main/vue/store/currentPoll.js
@@ -6,8 +6,11 @@ const currentPoll = {
     },
 
     mutations: {
-        set({state}, newPoll) {
+        set(state, newPoll) {
             state.poll = newPoll
+        },
+        update(state, pollCmd) {
+            Object.assign(state.poll, pollCmd)
         }
     },
 
@@ -15,12 +18,23 @@ const currentPoll = {
         load({commit}, id) {
             return new Promise((resolve, reject) => {
                 api.poll.get(id).then(function (res) {
-                    commit('setCurrentPoll', res.data);
+                    commit('set', res.data);
                     resolve(res.data);
                 }).catch(function (error) {
                     console.log(error);
                     reject();
                 });
+            });
+        },
+        update({commit}, pollCmd) {
+            return new Promise((resolve, reject) => {
+                commit('update', pollCmd)
+                api.poll.update(pollCmd)
+                    .then((res) => res.data)
+                    .catch((error) => {
+                        console.log(error);
+                        reject(error);
+                    })
             });
         }
     },
