3d2f2fcbad25e80999e032dddb0b5dcdd622e917
Ann-Cathrin <agroba@techfak.de>
2020-06-02 14:37:16 +0200

    Delete and Edit Button added and Sorting and Searching added, refs #14866 @3.0h

--- src/main/vue/pages/Admin.vue
+++ src/main/vue/pages/Admin.vue
@@ -1,136 +1,270 @@
 <template>
-    <!-- USE ONE OF THOSE TO YOU MEANS AS MOTHER -->
-
-
-        <div class="my-sh">
-            <nav-bar></nav-bar>
-            <b-row>
-                <p style="margin-left: 20vw; margin-top: 2vh"></p>
-            </b-row>
+    <div class="my-sh">
+        <nav-bar></nav-bar>
+        <b-row>
+            <p style="margin-left: 20vw; margin-top: 2vh"></p>
+        </b-row>
         <b-row align-h="center" style="margin-top: 5vh; margin-bottom: 5vh">
             <h1>User-Configuration</h1>
         </b-row>
-            <b-col >
-                <b-button class="addButton" v-b-modal.modal-1>+</b-button>
-            </b-col>
-            <b-modal @ok="addNewUser"
-                     centered
-                     id="modal-1"
-                     role="dialog"
-                     title="Add a new User">
-                <div class="modal-content">
-                    <div class="modal-body" style="padding:40px 50px;">
-                        <form role="form">
-                            <div class="form-group">
-                                <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
-                                <input class="form-control" id="usrname" placeholder="Enter Username" type="text">
-                            </div>
-                            <div class="form-group">
-                                <label for="password"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
-                                <input class="form-control" id="password" placeholder="Enter Password" type="text">
-                            </div>
-                            <div class="form-group">
-                                <label for="fullname"><span class="glyphicon glyphicon-eye-open"></span> Fullname</label>
-                                <input class="form-control" id="fullname" placeholder="Enter Fullname" type="text">
-                            </div>
-                            <div class="form-group">
-                                <label for="mail"><span class="glyphicon glyphicon-eye-open"></span> E-Mail-Adress</label>
-                                <input class="form-control" id="mail" placeholder="Enter Mail" type="text">
-                            </div>
-                            <div class="form-group">
-                                <b-dropdown id="role"
-                                            text="Select Role"
-                                            class="m-2">
-                                    <b-dropdown-item>Admin</b-dropdown-item>
-                                    <b-dropdown-item>Creator</b-dropdown-item>
-                                    <b-dropdown-item>Editor</b-dropdown-item>
-                                </b-dropdown>
-                            </div>
-                        </form>
-                    </div>
+        <b-col >
+            <b-button class="addButton" v-b-modal.modal-1>+</b-button>
+        </b-col>
+        <!---b-modal for Adding a new User--->
+        <b-modal @ok="addNewUser"
+                 centered
+                 id="modal-1"
+                 role="dialog"
+                 title="Add a new User">
+            <div class="modal-content">
+                <div class="modal-body" style="padding:40px 50px;">
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+                            <input class="form-control" id="usrname" placeholder="Enter Username" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="password"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+                            <input class="form-control" id="password" placeholder="Enter Password" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="fullname"><span class="glyphicon glyphicon-eye-open"></span> Fullname</label>
+                            <input class="form-control" id="fullname" placeholder="Enter Fullname" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="mail"><span class="glyphicon glyphicon-eye-open"></span> E-Mail-Adress</label>
+                            <input class="form-control" id="mail" placeholder="Enter Mail" type="text">
+                        </div>
+                        <div class="form-group">
+                            <b-dropdown id="role"
+                                        text="Select Role"
+                                        class="m-2">
+                                <b-dropdown-item>Admin</b-dropdown-item>
+                                <b-dropdown-item>Creator</b-dropdown-item>
+                                <b-dropdown-item>Editor</b-dropdown-item>
+                            </b-dropdown>
+                        </div>
+                    </form>
                 </div>
-            </b-modal>
+            </div>
+        </b-modal>
+    <b-container fluid>
+        <b-row>
+            <b-col lg="6" class="my-1">
+                <b-form-group
+                    label="Sort"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    label-for="sortBySelect"
+                    class="mb-0"
+                >
+                    <b-input-group size="sm">
+                        <b-form-select v-model="sortBy" id="sortBySelect" :options="sortOptions" class="w-75">
+                            <template v-slot:first>
+                                <option value="">-- none --</option>
+                            </template>
+                        </b-form-select>
+                        <b-form-select v-model="sortDesc" size="sm" :disabled="!sortBy" class="w-25">
+                            <option :value="false">Asc</option>
+                            <option :value="true">Desc</option>
+                        </b-form-select>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
 
-            <b-table
-                :fields="fields"
-                :items="items"
-                caption-top
-                bordered
-                outlined
-            sticky-header="true">
+            <b-col lg="6" class="my-1">
+                <b-form-group
+                    label="Initial sort"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    label-for="initialSortSelect"
+                    class="mb-0"
+                >
+                    <b-form-select
+                        v-model="sortDirection"
+                        id="initialSortSelect"
+                        size="sm"
+                        :options="['asc', 'desc', 'last']"
+                    ></b-form-select>
+                </b-form-group>
+            </b-col>
 
-                <template v-slot:table-caption>User</template>
+            <b-col lg="6" class="my-1">
+                <b-form-group
+                    label="Filter"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    label-for="filterInput"
+                    class="mb-0"
+                >
+                    <b-input-group size="sm">
+                        <b-form-input
+                            v-model="filter"
+                            type="search"
+                            id="filterInput"
+                            placeholder="Type to Search"
+                        ></b-form-input>
+                        <b-input-group-append>
+                            <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
 
-            </b-table>
+            <b-col lg="6" class="my-1">
+                <b-form-group
+                    label="Filter On"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    description="Leave all unchecked to filter on all data"
+                    class="mb-0">
+                    <b-form-checkbox-group v-model="filterOn" class="mt-1">
+                        <b-form-checkbox value="username">Name</b-form-checkbox>
+                        <b-form-checkbox value="mail">Age</b-form-checkbox>
+                        <b-form-checkbox value="role">Role</b-form-checkbox>
+                    </b-form-checkbox-group>
+                </b-form-group>
+            </b-col>
 
-        </div>
+            <b-col sm="5" md="6" class="my-1">
+                <b-form-group
+                    label="Per page"
+                    label-cols-sm="6"
+                    label-cols-md="4"
+                    label-cols-lg="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    label-for="perPageSelect"
+                    class="mb-0"
+                >
+                    <b-form-select
+                        v-model="perPage"
+                        id="perPageSelect"
+                        size="sm"
+                        :options="pageOptions"
+                    ></b-form-select>
+                </b-form-group>
+            </b-col>
 
+            <b-col sm="7" md="6" class="my-1">
+                <b-pagination
+                    v-model="currentPage"
+                    :total-rows="totalRows"
+                    :per-page="perPage"
+                    align="fill"
+                    size="sm"
+                    class="my-0"
+                ></b-pagination>
+            </b-col>
+        </b-row>
 
+        <!-- Main table element -->
+        <b-table
+            show-empty
+            small
+            stacked="md"
+            :items="items"
+            :fields="fields"
+            :current-page="currentPage"
+            :per-page="perPage"
+            :filter="filter"
+            :filterIncludedFields="filterOn"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :sort-direction="sortDirection"
+            @filtered="onFiltered"
+        >
+            <template v-slot:cell(name)="row">
+                {{ row.value.username }}
+            </template>
+
+            <template v-slot:cell(actions)="row">
+                <b-button size="sm" @click="row.toggleDetails">
+                    Edit
+                </b-button>
+                <b-button size="sm" variant="danger" @click="deleteUser(row.item)" class="mr-1">
+                    Delete
+                </b-button>
+            </template>
+
+            <template v-slot:row-details="row">
+                <b-card>
+                    <ul>
+                        <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+                    </ul>
+                </b-card>
+            </template>
+
+            <!----b-modal for edit--->
+
+        </b-table>
+
+    </b-container>
+    </div>
 </template>
 
 <script>
     import {mapActions, mapGetters} from "vuex";
     //import axios from "axios";
     import NavBar from "../components/NavBar";
-
     export default {
-
-        // Order is related to its use
         name: "Admin",
-        // passed variables by e.g v-bind
-        props: ["user"],
-        // variables provided by this component
         data() {
             return {
-                fields: [
-                    {
-                        key: 'username',
-                        label: 'Username',
-                        sortable: true
-                    },
-                    {
-                        key: 'mail',
-                        label: 'E_Mail_Adress',
-                        sortable: true
-                    },
-                    {
-                        key: 'role',
-                        label: 'Role',
-                        sortable: false
-                    },
-                ],
-                filter: null,
-                filterOn: [],
-
                 items: [
 
-                    {Username: 'JamesBond', E_Mail_Adress: 'jbond@mi6.com', Role: 'Admin'},
+                    {username: 'JamesBond', mail: 'jbond@mi6.com', role: 'Admin'},
 
-                    {Username: 'JamesMartin', E_Mail_Adress: 'jmartin@web.com', Role: 'Creator'}
+                    {username: 'JamesMartin', mail: 'jmartin@web.com', role: 'Creator'}
 
-                ]
-
+                ],
+                fields: [
+                    { key: 'username', label: 'Username', sortable: true, sortDirection: 'desc' },
+                    { key: 'mail', label: 'E-Mail', sortable: true, class: 'text-center' },
+                    { key: 'role', label: 'Role', sortable: true, class: 'text-center' },
+                    { key: 'actions', label: 'Actions' }
+                ],
+                totalRows: 1,
+                currentPage: 1,
+                perPage: 5,
+                pageOptions: [5, 10, 15],
+                sortBy: '',
+                sortDesc: false,
+                sortDirection: 'asc',
+                filter: null,
+                filterOn: [],
+                infoModal: {
+                    id: 'info-modal',
+                    title: '',
+                    content: ''
+                }
             }
         },
-
-        // Vue lifecycle related functions, if needed
-        beforeMount() {
+        computed: {
+            ...mapGetters([]),
+            sortOptions() {
+                // Create an options list from our fields
+                return this.fields
+                    .filter(f => f.sortable)
+                    .map(f => {
+                        return { text: f.label, value: f.key }
+                    })
+            }
         },
         mounted() {
-            this.$root.$on('bv::dropdown::show', bvEvent => {
-                console.log('Dropdown is about to be shown', bvEvent)
-            })
-        },
-        beforeCreate() {
-        },
-        created() {
-        },
-
-        // ...maps at top, functions provided by store
-        computed: {
-            ...mapGetters([])
+            // Set the initial number of items
+            this.totalRows = this.items.length
         },
         methods: {
+            info(item, index, button) {
+                this.infoModal.title = `Row index: ${index}`
+                this.infoModal.content = JSON.stringify(item, null, 2)
+                this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+            },
             ...mapActions([]),
             // write methods to your means
             addNewUser() {
@@ -138,16 +272,20 @@
             },
             deleteUser() {
 
-            }
-
-        },
-
-        // after import, components need to be listed here as well
-        comments: {
-            NavBar,
-        },
-
-
+            },
+            resetInfoModal() {
+                this.infoModal.title = ''
+                this.infoModal.content = ''
+            },
+            onFiltered(filteredItems) {
+                // Trigger pagination to update the number of buttons/pages due to filtering
+                this.totalRows = filteredItems.length
+                this.currentPage = 1
+            },
+            comments: {
+                NavBar,
+            },
+        }
     }
 </script>
 
@@ -159,5 +297,3 @@
         height: 50px;
     }
 </style>
-
-
