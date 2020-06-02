9149249d6ad84e9a991132e9d39b3a45873ec206
Ann-Cathrin <agroba@techfak.de>
2020-06-02 13:10:22 +0200

    Trying to add a filter function, refs#14866, @2.0h

--- src/main/vue/pages/Admin.vue
+++ src/main/vue/pages/Admin.vue
@@ -1,8 +1,9 @@
 <template>
     <!-- USE ONE OF THOSE TO YOU MEANS AS MOTHER -->
 
-    <div class="my-back">
+
         <div class="my-sh">
+            <nav-bar></nav-bar>
             <b-row>
                 <p style="margin-left: 20vw; margin-top: 2vh"></p>
             </b-row>
@@ -36,29 +37,34 @@
                                 <label for="mail"><span class="glyphicon glyphicon-eye-open"></span> E-Mail-Adress</label>
                                 <input class="form-control" id="mail" placeholder="Enter Mail" type="text">
                             </div>
-                            <div class="dropdown show">
-                                <div aria-labelledby="role" class="dropdown-menu">
-                                   <a class="dropdown-item" href="#">Admin</a>
-                                    <a class="dropdown-item" href="#">Creator</a>
-                                    <a class="dropdown-item" href="#">Editor</a>
-                                </div>
+                            <div class="form-group">
+                                <b-dropdown id="role"
+                                            text="Select Role"
+                                            class="m-2">
+                                    <b-dropdown-item>Admin</b-dropdown-item>
+                                    <b-dropdown-item>Creator</b-dropdown-item>
+                                    <b-dropdown-item>Editor</b-dropdown-item>
+                                </b-dropdown>
                             </div>
-                            <b-dropdown>
-                                <b-dropdown-item>Admin</b-dropdown-item>
-                                <b-dropdown-item>Creator</b-dropdown-item>
-                            </b-dropdown>
                         </form>
                     </div>
                 </div>
             </b-modal>
 
+            <b-table
+                :fields="fields"
+                :items="items"
+                caption-top
+                bordered
+                outlined
+            sticky-header="true">
 
-        <b-table :fields="fields" :items="items" caption-top>
-            <template v-slot:table-caption>User</template>
+                <template v-slot:table-caption>User</template>
 
-        </b-table>
+            </b-table>
+
         </div>
-    </div>
+
 
 </template>
 
@@ -76,11 +82,34 @@
         // variables provided by this component
         data() {
             return {
-                fields: ['Username', 'E_Mail_Adress', 'Role'],
+                fields: [
+                    {
+                        key: 'username',
+                        label: 'Username',
+                        sortable: true
+                    },
+                    {
+                        key: 'mail',
+                        label: 'E_Mail_Adress',
+                        sortable: true
+                    },
+                    {
+                        key: 'role',
+                        label: 'Role',
+                        sortable: false
+                    },
+                ],
+                filter: null,
+                filterOn: [],
+
                 items: [
+
                     {Username: 'JamesBond', E_Mail_Adress: 'jbond@mi6.com', Role: 'Admin'},
+
                     {Username: 'JamesMartin', E_Mail_Adress: 'jmartin@web.com', Role: 'Creator'}
+
                 ]
+
             }
         },
 
@@ -88,6 +117,9 @@
         beforeMount() {
         },
         mounted() {
+            this.$root.$on('bv::dropdown::show', bvEvent => {
+                console.log('Dropdown is about to be shown', bvEvent)
+            })
         },
         beforeCreate() {
         },
@@ -125,10 +157,6 @@
         font-size: 140%;
         width: 50px;
         height: 50px;
-    }
-    .my-back {
-        min-height: 100vh;
-        background-color: lightgray
     }
 </style>
 
