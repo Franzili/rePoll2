171d19f3201a4a2e4538d0c517edbd0316b48b3e
lkrueger <lkrueger@techfak.uni-bielefeld.de>
2020-06-05 10:34:58 +0200

    Fixed 1 checkstyle problem

--- src/main/java/gpse/repoll/domain/service/QuestionService.java
+++ src/main/java/gpse/repoll/domain/service/QuestionService.java
@@ -30,6 +30,8 @@ public interface QuestionService {
      * @param scaleNameLeft The name for the left part of the scale
      * @param scaleNameRight The name for the right part of the scale
      * @param stepCount The number of steps the scale has
+     * @param min The minimum of the scale
+     * @param max  The maximum of the scale
      * @return The newly created ScaleQuestion
      * @throws gpse.repoll.domain.exceptions.NotFoundException If the corresponding Poll could not be found.
      */
