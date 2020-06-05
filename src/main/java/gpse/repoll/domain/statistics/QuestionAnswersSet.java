37edf4b3b5a55ac104ba5f64499c455d63802ad9
lkrueger <lkrueger@techfak.uni-bielefeld.de>
2020-06-05 14:57:41 +0200

    Created a new class QuestionAnswersSet for easy access to the answers Created new GET-Mapping that provides a list of these objects for a Poll refs #15193

---
+++ src/main/java/gpse/repoll/domain/statistics/QuestionAnswersSet.java
@@ -0,0 +1,40 @@
+package gpse.repoll.domain.statistics;
+
+import gpse.repoll.domain.User;
+import gpse.repoll.domain.poll.answers.Answer;
+import gpse.repoll.domain.poll.questions.Question;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionAnswersSet {
+
+    private Question question;
+    private final Map<User, Answer> userAnswerMap = new HashMap<>();
+
+    public QuestionAnswersSet(Map<User, Answer> userAnswerMap, Question question) {
+        setQuestion(question);
+        setUserAnswerMap(userAnswerMap);
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public void setUserAnswerMap(Map<User, Answer> userAnswerMap) {
+        this.userAnswerMap.clear();
+        this.userAnswerMap.putAll(userAnswerMap);
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public Map<User, Answer> getUserAnswerMap() {
+        return userAnswerMap;
+    }
+
+    public void add(User user, Answer answer) {
+        userAnswerMap.put(user, answer);
+    }
+}
