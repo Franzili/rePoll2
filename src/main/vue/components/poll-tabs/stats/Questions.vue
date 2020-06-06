e2bdb74ea8d5dfe684425501760ce5272ac1e85d
Maximilian Martini <mmartini@techfak.uni-bielefeld.de>
2020-06-06 18:56:05 +0200

    hid some prototype ideas for deeper analyses, added header, added b-rows and b-container for design changes #14862 @1.5h

--- src/main/vue/components/poll-tabs/stats/Questions.vue
+++ src/main/vue/components/poll-tabs/stats/Questions.vue
@@ -1,32 +1,52 @@
 <template>
-    <div>
+    <b-container>
         <p>
             <b-row style="margin-top: 2vh">
+
+                <!-- TODO select for serial questions
                 <b-col cols="4">
-                    <b-form-select v-model="selected" :options="this.choices"></b-form-select>
+                    <b-form-select></b-form-select>
                 </b-col>
-                <b-col cols="4">
-                    <b-form-select v-model="selected" :options="this.choices"></b-form-select>
+                -->
+
+                <b-col cols="8">
+                    <b-form-select v-model="selected" :options="this.qSelecter"></b-form-select>
                 </b-col>
                 <b-col cols="4"></b-col>
             </b-row>
         </p>
 
-
-        <p> Hallo ich bin ausgewählt: {{selected}}</p>
+        <!-- TODO Prototype for deeper analyses
         <b-card v-if="selQuest.length > 0">
-            <b-row v-bind:key="question.id" v-for="question in selQuest" >
-                <b-col><h5>{{question.title}}</h5></b-col>
-                <b-col><b-button>x</b-button></b-col>
+            <b-row v-bind:key="question.id" v-for="question in selQuest" class="align-items-center" >
+                <b-col><h6>{{question.title}}</h6></b-col>
+                <b-col><b-button
+                    class="float-right"
+                    variant="outline-secondary"
+                    pill
+                    size="sm"
+                    @click="deleteSelected(question.id)"
+                >x</b-button></b-col>
             </b-row>
         </b-card>
-
-        <!--
-        <p>{{answerSet}}</p>
         -->
+        <b-row v-if="selected !==null">
+            <h6>{{(poll.questions.find(question => question.id === selected).title)}}</h6>
+        </b-row>
 
-        <b-table v-if="selected !== null" striped hover :items="answerSet" fixed :fields="fields" ></b-table>
-    </div>
+        <b-row>
+            <!--TODO Prototype for deeper analyses, "&& selQuest.length > 0"-->
+            <b-table v-if="selected !== null"
+                     striped
+                     hover
+                     fixed
+                     outlined
+                     :items="answerSet"
+                     :fields="fields"
+            ></b-table>
+        </b-row>
+
+    </b-container>
 </template>
 
 <script>
@@ -37,14 +57,15 @@
         name: "Questions",
         data() {
             return {
-                choices: [],
+                qSelecter: [{value: null, text: 'Select a question for display'}],
                 selected: null,
                 answerSet: [],
-                selQuest: [],
                 fields: [
                     {key: 'Username', sortable: true},
                     {key: 'Answers', sortable: true}
-                ]
+                ],
+                // TODO Prototype for deeper analyses
+                //selQuest: [],
             }
         },
         created() {
@@ -62,15 +83,32 @@
         },
         watch: {
             selected: function (val) {
-                this.answerSet = this.getPollAnswers(val)
-                this.doFuckingFunnyStuff(val)
+                if (val !== null) {
+                    this.answerSet = this.getPollAnswers(val)
+                } else {
+                    this.answerSet = []
+                }
+
+
+                // TODO Prototype for deeper analyses
+                //this.addQuestToSelected(val)
             }
         },
         methods: {
             ...mapActions('currentPoll', {
                 loadPollAnswers: 'loadPollAnswers'
             }),
-            doFuckingFunnyStuff(val) {
+            // TODO Prototype for deeper analyses
+            /*deleteSelected(id) {
+                let tmpQuests = []
+                for (let i = 0; i < this.selQuest.length; i++) {
+                    if (this.selQuest[i].id !== id) {
+                        tmpQuests.push(this.selQuest[i])
+                    }
+                }
+                this.selQuest = tmpQuests
+            },
+            addQuestToSelected(val) {
                 let question = this.poll.questions.find(question => question.id === val)
                 console.log(this.selQuest.length > 0)
                 if (this.selQuest.length > 0) {
@@ -80,11 +118,13 @@
                 } else {
                     this.selQuest.push(question)
                 }
-            },
+            },*/
+
+
             fillChoices() {
                 for (let i = 0; i < this.poll.questions.length; i++) {
                     let choice = {text: this.poll.questions[i].title, value: this.poll.questions[i].id}
-                    this.choices = [... this.choices, choice]
+                    this.qSelecter = [... this.qSelecter, choice]
                 }
             }
         },
