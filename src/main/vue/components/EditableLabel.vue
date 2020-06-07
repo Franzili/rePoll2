b142a5615dedb24912e7c67445d93bddaed9a801
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-06-07 08:54:50 +0200

    Added EditableLabel refs #15167 @0.3h

---
+++ src/main/vue/components/EditableLabel.vue
@@ -0,0 +1,41 @@
+<template>
+    <component v-if="!editing" :is="tag">{{value}}</component>
+    <input v-else v-model="currentValue"/>
+</template>
+
+<script>
+    export default {
+        name: "EditableLabel",
+        data() {
+            return {
+                currentValue: this.value
+            }
+        },
+        props: {
+            tag: {
+                required: false,
+                type: String,
+                default: "span"
+            },
+            value: {
+                required: false,
+                type: String,
+                default: ""
+            },
+            editing: {
+                required: false,
+                type: Boolean,
+                default: false
+            }
+        },
+        watch: {
+            currentValue: function() {
+                this.$emit('valueChanged', this.currentValue)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
