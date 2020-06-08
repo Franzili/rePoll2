edb22be01e4e52f463c4af35845a984ae8687a53
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-06-08 19:17:47 +0200

    Answer page of polls shows poll elements refs #14187 @1.8

--- src/main/vue/pages/Answer.vue
+++ src/main/vue/pages/Answer.vue
@@ -1,49 +1,100 @@
 <template>
-    <div style="text-align:center;">
-    <b-container class="mt-3">
-        <b-row>
-            <b-col>
-                    <b-form @submit.prevent="savePollEntry">
-                        <b-form-group>
-                            <p class="my-name">{{poll.title}}</p>
-                        </b-form-group>
-                        <b-row>
-                            <b-col></b-col>
-                            <b-form-group>
+    <!--
+        Renders a poll.
+    -->
+    <b-container>
+        <ul class="poll-main-view">
+            <PollItem v-for="item in pollStructure"
+                      v-bind:key="item.id"
+                      :model="item"
+                      :id="'pollItem-' + item.id"
+                      :editable="false"
+                      :hide-border="true"
+            />
+        </ul>
 
-                                <div :key="question.id" v-for="question in questions">
-
-                                <!--
-                                <div :key="item.id" v-for="item in items">
-                                    -->
-                                    <PollQuestion v-bind:question="question" v-bind:edit="edit"/>
-                                </div>
-                            </b-form-group>
-                            <b-col></b-col>
-                        </b-row>
-                        <!--
-                        template save buttons!!!
-                        <b-button variant="primary" type="submit">Speichern</b-button>
-                        -->
-
-
-                        <!-- old save button
-                        <b-button @click="answerPoll">Save</b-button>
-                        -->
-                        <b-button variant="primary" v-on:click="savePollEntry">Save</b-button>
-                        <!--
-                        Submit Button for later
-                        Final Submit, then answers can't be edited animore
-                        <b-button class="my-button" variant="success">Submit!</b-button>
-                        -->
-                    </b-form>
-            </b-col>
-        </b-row>
+        <b-button variant="primary" v-on:click="answerPoll">Save</b-button>
+        <!--
+        Submit Button for later
+        Final Submit, then answers can't be edited animore
+        <b-button class="my-button" variant="success">Submit!</b-button>
+        -->
     </b-container>
-    </div>
+
 </template>
 
+<script>
+    import {mapGetters, mapActions} from "vuex"
 
+    import PollItem from "../components/poll-tabs/edit/poll-items/PollItem";
+    //import axios from "axios";
+
+    export default {
+        name: "Answer",
+        data() {
+            return {
+                currentlyEditing: null
+            }
+        },
+        computed: {
+            ...mapGetters('currentPoll', {
+                pollStructure: 'pollStructureFlat'
+            })
+        },
+        methods: {
+            ...mapActions('currentPoll', {
+                loadPoll: 'load'
+            }),
+            //on button save:
+            //get children
+            //put children in Entry'sCmd
+            //sent to api
+
+            answerPoll() {
+                //TODO is this the first time answering this poll, then "answerFirst(poll)" else "answerAgain(poll) in polls.js"
+
+                let pollAnswers = document.querySelector('.poll-main-view').children;
+                console.log('pollAnswers: ', pollAnswers)
+                for (let i=0; i < pollAnswers.length; i++) {
+                    console.log('pollAnswers: ', pollAnswers[i])
+                }
+
+                /*if (!(this.status.equals("filledOut"))) { //first time filling out  //("ready")
+                    this.status.set("filledOut")
+                    // write answers
+                    for (let i = 0; i < this.questions.length; i++) {
+                        axios.post(
+                            '/api/v1/polls/' + this.id + '/entries/' + this.entries[i].id + '/',
+                            this.entries[i])
+                    }
+                } else if (this.status.equals("filledOut")) { //every following time filling out
+                    // update answers
+                    for (let j = 0; j < this.questions.length; j++) {
+                        axios.put(
+                            '/api/v1/polls/' + this.id + '/entries/' + this.entries[j].id + '/',
+                            this.entries[j])
+                    }
+                */
+            },
+        },
+        created() {
+            let pollId = this.$route.params.id
+            this.loadPoll(pollId)
+            console.log(this.$route)
+        },
+        components: { PollItem }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .poll-main-view {
+        // turn off default list styling
+        list-style-type: none;
+        padding-left: 0;
+    }
+</style>
+
+<!--
 <script>
     import {mapGetters, mapMutations} from "vuex";
     import PollQuestion from "../components/PollQuestion";
@@ -106,3 +157,4 @@
 
 <style scoped>
 </style>
+-->
