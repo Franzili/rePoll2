777420c5bda04d56db003ea0666c4512105ba353
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-06-08 11:42:03 +0200

    removed AnswerPoll.vue and all references on it

--- src/main/vue/components/NavBar.vue
+++ src/main/vue/components/NavBar.vue
@@ -26,9 +26,6 @@
                     <b-nav-item>
                         <router-link class="link" :to="'/create/'">Create Poll</router-link>
                     </b-nav-item>
-                    <b-nav-item>
-                        <router-link class="link" :to="'/answer/'">Answer Poll</router-link>
-                    </b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
