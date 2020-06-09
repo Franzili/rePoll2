deebab2e81e8c836cb57dbcff2249013f98c54a6
Michael Peresada <mperesada@techfak.uni-bielefeld.de>
2020-06-09 15:25:12 +0200

    refs #14860 the data is now displayed readably @0.25h

--- src/main/vue/components/poll-tabs/stats/Entries.vue
+++ src/main/vue/components/poll-tabs/stats/Entries.vue
@@ -11,27 +11,28 @@
         </b-container>
 
         <div v-if="getEntries(selected)[0]">
-            <!--<b-container>
+            <b-container>
                 <b-row>
                     <b-col><h6>Questions</h6></b-col>
                     <b-col><h6>Answers</h6></b-col>
                 </b-row>
-                <div v-bind:key="answer.qId" v-for="answer in getEntries(selected)">
-                    <b-row>
-                        <b-col>
-                            <p> {{answer.question}} </p>
-                        </b-col>
-                        <b-col>
-                            <p> {{answer.answer}} </p>
-                        </b-col>
-                    </b-row>
+
+                <div v-bind:key="section.section" v-for="section in getEntries(selected)">
+                    <h6>{{section.section}}</h6>
+                    <div v-if="section.idQA[0]">
+                        <div v-bind:key="idQA2.qId" v-for="idQA2 in section.idQA">
+                            <b-row>
+                                <b-col>
+                                    <p> {{idQA2.question}} </p>
+                                </b-col>
+                                <b-col>
+                                    <p> {{idQA2.answer}} </p>
+                                </b-col>
+                            </b-row>
+                        </div>
+                    </div>
                 </div>
-            </b-container>-->
-
-            <p>
-                {{getEntries(selected)}}
-            </p>
-
+            </b-container>
         </div>
     </div>
 </template>
