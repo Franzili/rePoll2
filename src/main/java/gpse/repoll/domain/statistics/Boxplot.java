12a6f875aa1596ac0d8a29e9a53b320d842978d9
lkrueger <lkrueger@techfak.uni-bielefeld.de>
2020-06-11 10:26:04 +0200

    Fixed calculation for the boxplot (median and quatiles) Quartiles class renamed to Boxplot and added min and max to it

---
+++ src/main/java/gpse/repoll/domain/statistics/Boxplot.java
@@ -0,0 +1,51 @@
+package gpse.repoll.domain.statistics;
+
+/**
+ * First and third quartile of frequencies to a corresponding question.
+ */
+public class Boxplot {
+
+    protected int min;
+    protected Double firstQuartile;
+    protected Double thirdQuartile;
+    protected int max;
+
+    protected Boxplot(int min, Double firstQuartile, Double thirdQuartile, int max) {
+        this.min = min;
+        this.firstQuartile  = firstQuartile;
+        this.thirdQuartile = thirdQuartile;
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public Double getFirstQuartile() {
+        return firstQuartile;
+    }
+
+    public void setFirstQuartile(Double firstQuartile) {
+        this.firstQuartile = firstQuartile;
+    }
+
+    public Double getThirdQuartile() {
+        return thirdQuartile;
+    }
+
+    public void setThirdQuartile(Double thirdQuartile) {
+        this.thirdQuartile = thirdQuartile;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+}
