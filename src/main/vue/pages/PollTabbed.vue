b22a862c572aa465f33b736e29fe489ba0ffc29b
Maximilian Martini <mmartini@techfak.uni-bielefeld.de>
2020-06-12 21:01:38 +0200

    deleted old stuff #15285 @0.05h

--- src/main/vue/pages/PollTabbed.vue
+++ src/main/vue/pages/PollTabbed.vue
@@ -36,94 +36,20 @@
                 <router-view></router-view>
             </b-col>
         </b-row>
-        <!--
-        <b-row>
-            <b-col>
-                <b-tabs lazy nav-class="invisible" v-model="activeTab">
-                    <b-tab>
-                        <ConfigurePoll></ConfigurePoll>
-                    </b-tab>
-                    <b-tab>
-                        <CreatePoll></CreatePoll>
-                    </b-tab>
-                    <b-tab>
-                        <PollStats></PollStats>
-                    </b-tab>
 
-                </b-tabs>
-            </b-col>
-        </b-row>
-        -->
     </b-container>
 </template>
 
 <script>
-    /*
-    how backend polls are provided here:
-    poll() <- poll object
-    easy access:
-    poll.title (in js with this. ..)
-    poll.id    (in js with this. ..)
-    where can i see poll objects?
-    in your browser with vue plugin, an empty poll could look like this:
-    (in your vue dev plugin)
-    computed
-        poll:
-            creationTime:
-            creator:
-                email:
-                fullName:
-                id:
-                ownPolls: []
-                username:
-            entries: []
-            id:
-            lastEditTime:
-            lastEditor:
-            owner:
-            questions: [
-                0:
-                    charLimit:
-                    id:
-                    questionOrder:
-                    title:
-                    type:
-            ]
-            sections: []
-            status:
-            title:
-    */
 
-    //import ConfigurePoll from "../components/poll-tabs/configure/ConfigurePoll";
-    //import CreatePoll from "../components/poll-tabs/edit/EditPoll";
-    //import PollStats from "../components/poll-tabs/stats/PollStats";
     import {mapState} from "vuex";
     export default {
         name: "PollTabbed",
-        data() {
-            return {
-                activeTab: 0,
-                TAB_CONFIGURE: 0,
-                TAB_EDIT: 1,
-                TAB_STATISTICS: 2,
-                bla: true
-            }
-        },
         computed: {
             ...mapState('currentPoll', {
                 poll: 'poll'
             })
         },
-        /*methods: {
-            ...mapActions('currentPoll', {
-                loadPoll: 'load',
-            })
-        },
-        async mounted() {
-            let pollId = this.$route.params.pollId
-            await this.loadPoll(pollId)
-        },*/
-        //components: {ConfigurePoll, CreatePoll, PollStats}
     }
 </script>
 
