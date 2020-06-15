d88aac78d306ff74ec2419ad119a48fab0cd61c8
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-06-15 09:31:17 +0200

    Added WebConfiguration to enable serving of static files. (i.e. html css js in production) w/ Leonie & Max @0.5h

---
+++ src/main/java/gpse/repoll/WebConfiguration.java
@@ -0,0 +1,21 @@
+package gpse.repoll;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configures Spring to handle all HTTP Requests that are not prefixed by /api/v1/
+ * by serving the static html, js, css files.
+ *
+ * '@RequestMapping' Annotations have higher priority than this, so they are not overridden
+ * by this config.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("**").setViewName("forward:/");
+    }
+}
