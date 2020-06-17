45cf6a7a25a31837edb0baf4001765936ee813d8
Ann-Cathrin <agroba@techfak.de>
2020-06-17 13:30:05 +0200

    Class Participant and Repository added, refs #15464 @1.5h

---
+++ src/main/java/gpse/repoll/web/command/ParticipantCmd.java
@@ -0,0 +1,27 @@
+package gpse.repoll.web.command;
+
+public class ParticipantCmd {
+
+    private String fullName;
+    private String email;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+}
