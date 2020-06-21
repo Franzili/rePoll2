77bdd7077d1d3b065c59bca2be3e07b9e4ce028a
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-06-21 17:13:26 +0200

    added in /api iteration.js refs: #15704 @0.3

---
+++ src/main/vue/api/iterations.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default {
+    listAll(pollId) {
+        return axios.get('/api/v1/polls/' + pollId + '/');
+    },
+    getIter(pollId, iterId) {
+        return axios.get('api/v1/polls/' + pollId + '/iterations/' + iterId + '/')
+    },
+    addIter(pollId,iterationCmd) {
+        return axios.post('/api/v1/polls/' + pollId + '/iterations/', iterationCmd);
+    },
+    removeIter(pollId, iterId) {
+        return axios.delete('api/v1/polls/' + pollId + '/iterations/' + iterId + '/')
+    },
+    updateIter(pollId, iteration) {
+        return axios.put('/api/v1/polls/' + pollId + '/questiiterationsons/' + iteration.id + '/', iteration);
+    }
+}
