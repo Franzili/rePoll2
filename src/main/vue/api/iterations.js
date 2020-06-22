8621d46384be7d9325394c514c114c57a5f38d8a
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-06-22 21:14:02 +0200

    fixed typos

--- src/main/vue/api/iterations.js
+++ src/main/vue/api/iterations.js
@@ -5,13 +5,13 @@ export default {
         return axios.get('/api/v1/polls/' + pollId + '/');
     },
     getIter(pollId, iterId) {
-        return axios.get('api/v1/polls/' + pollId + '/iterations/' + iterId + '/')
+        return axios.get('api/v1/polls/' + pollId + '/iterations/' + iterId + '/');
     },
     addIter(pollId,iterationCmd) {
         return axios.post('/api/v1/polls/' + pollId + '/iterations/', iterationCmd);
     },
     removeIter(pollId, iterId) {
-        return axios.delete('api/v1/polls/' + pollId + '/iterations/' + iterId + '/')
+        return axios.delete('api/v1/polls/' + pollId + '/iterations/' + iterId + '/');
     },
     updateIter(pollId, pollIterationCmd) {
         return axios.put('/api/v1/polls/' + pollId + '/iterations/' + pollIterationCmd.id + '/', pollIterationCmd);
