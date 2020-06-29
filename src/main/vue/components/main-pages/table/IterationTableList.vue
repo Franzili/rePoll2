5440d74897f5e509ae58ada9f2d1235ff0077699
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2020-06-29 15:39:08 +0200

    changed from container to list-group

--- src/main/vue/components/main-pages/table/IterationTableList.vue
+++ src/main/vue/components/main-pages/table/IterationTableList.vue
@@ -1,27 +1,49 @@
 <template>
 
+
     <div>
-            <div v-bind:key="iteration.id" v-for="iteration in iterations">
-                <div class="list-group" id="IterationOPEN" role="tablist">
+        <div v-bind:key="iteration.id" v-for="iteration in iterations">
+            <div class="list-group" id="IterationOPEN" role="tablist">
+                <p v-if="iteration.status === 'OPEN'">
+                    <IterationTableElementOPEN v-bind:iteration="iteration"/>
+                </p>
+            </div>
+
+            <div class="list-group" id="IterationSCHEDULED" role="tablist">
+                <p v-if="iteration.status === 'SCHEDULED'">
+                    <IterationTableElementSCHEDULED v-bind:iteration="iteration"/>
+                </p>
+            </div>
+
+            <div class="list-group" id="IterationCLOSED" role="tablist">
+                <p v-if="iteration.status === 'CLOSED'">
+                    <IterationTableElementCLOSED v-bind:iteration="iteration"/>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!--
+        <b-container>
+            <b-row v-bind:key="iteration.id" v-for="iteration in iterations">
+                <b-col>
                     <p v-if="iteration.status === 'OPEN'">
                         <IterationTableElementOPEN v-bind:iteration="iteration"/>
                     </p>
-                </div>
-
-                <div class="list-group" id="IterationSCHEDULED" role="tablist">
+                </b-col>
+                <b-col>
                     <p v-if="iteration.status === 'SCHEDULED'">
                         <IterationTableElementSCHEDULED v-bind:iteration="iteration"/>
                     </p>
-                </div>
-
-                <div class="list-group" id="IterationCLOSED" role="tablist">
+                </b-col>
+                <b-col>
                     <p v-if="iteration.status === 'CLOSED'">
                         <IterationTableElementCLOSED v-bind:iteration="iteration"/>
                     </p>
-                </div>
-            </div>
-    </div>
-
+                </b-col>
+            </b-row>
+        </b-container>
+    -->
 
 </template>
 
@@ -29,18 +51,10 @@
     import IterationTableElementSCHEDULED from "../../main-pages/table/IterationTableElementSCHEDULED";
     import IterationTableElementOPEN from "../../main-pages/table/IterationTableElementOPEN";
     import IterationTableElementCLOSED from "../../main-pages/table/IterationTableElementCLOSED";
-    import {mapActions} from "vuex";
 
     export default {
         name: "IterationTableList",
         props: ["iterations"],
-        methods: {
-            ...mapActions('myIterations', {
-                createIteration: 'create',
-                listIterations: 'load',
-                deleteIteration: 'delete'
-            }),
-        },
         components: {IterationTableElementSCHEDULED,IterationTableElementOPEN, IterationTableElementCLOSED}
     }
 </script>
