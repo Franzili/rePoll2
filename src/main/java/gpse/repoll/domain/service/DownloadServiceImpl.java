ff7156562c4d14334987e538c6508a11c9ec9f07
Michael Peresada <mperesada@techfak.uni-bielefeld.de>
2020-06-30 14:57:25 +0200

    refs #16051 moved getHumanReadable function from Poll to DownloadServiceImpl @0.16h

--- src/main/java/gpse/repoll/domain/service/DownloadServiceImpl.java
+++ src/main/java/gpse/repoll/domain/service/DownloadServiceImpl.java
@@ -1,5 +1,8 @@
 package gpse.repoll.domain.service;
 
+import gpse.repoll.domain.poll.Poll;
+import gpse.repoll.domain.poll.PollSection;
+import gpse.repoll.domain.poll.questions.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
@@ -17,6 +20,35 @@ public class DownloadServiceImpl implements DownloadService {
     @Override
     public String download(UUID id, String type, String format) {
         //if (format.equals("human") && type.equals("poll"))
-        return pollService.getPoll(id).getAsHumanReadable();
+
+        Poll currentPoll = pollService.getPoll(id);
+
+        StringBuilder sectionsWithQuestions = new StringBuilder();
+
+        for (PollSection section : currentPoll.getPollSections()) {
+            sectionsWithQuestions.append(section.getTitle());
+            sectionsWithQuestions.append(": ");
+            sectionsWithQuestions.append("\n");
+            for (Question q : section.getQuestions()) {
+                sectionsWithQuestions.append(q.getTitle());
+                sectionsWithQuestions.append(", ");
+            }
+            if (section.getQuestions().isEmpty()) {
+                sectionsWithQuestions.append("\n");
+            } else {
+                //remove the last ','
+                sectionsWithQuestions.delete(sectionsWithQuestions.length() - 2, sectionsWithQuestions.length() - 1);
+                sectionsWithQuestions.append("\n \n");
+            }
+        }
+
+        return "Poll " + currentPoll.getTitle() + ":\n\n"
+            + "Id:        " + id + "\n"
+            + "Status:    " + currentPoll.getStatus() + "\n"
+            + "Anonymity: " + currentPoll.getAnonymity() + "\n\n\n"
+            + "Questions:\n" + sectionsWithQuestions.toString() + "\n";
+
+
+        //return pollService.getPoll(id).getAsHumanReadable();
     }
 }
