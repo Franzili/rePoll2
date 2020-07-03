ee973ca08b61710e9eb6280d73e2b74efcdff5ef
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-07-03 12:28:29 +0200

    Added Launch.vue; refs #15704 @0.2h

---
+++ src/main/vue/components/poll-tabs/configure/Launch.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-card>
+        <h6>Launch</h6>
+        <div class="d-flex align-items-center">
+            <span class="text-muted flex-grow-1">
+                If you launch the poll, you will not be able to edit it again.
+            </span>
+            <b-button class="button-spacing">
+                Schedule
+            </b-button>
+            <b-button variant="primary" class="button-spacing">
+                Launch now!
+            </b-button>
+        </div>
+    </b-card>
+</template>
+
+<script>
+    export default {
+        name: "Launch.vue"
+    }
+</script>
+
+<style scoped>
+    .button-spacing {
+        margin-left: 5px;
+    }
+
+</style>
