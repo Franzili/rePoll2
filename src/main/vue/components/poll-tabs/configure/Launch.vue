331b0c9e81982b3d64f886e7e6c25e684c57ac5d
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-07-03 17:20:35 +0200

    Implemetned proper launching of poll. refs #15704 @0.2h

--- src/main/vue/components/poll-tabs/configure/Launch.vue
+++ src/main/vue/components/poll-tabs/configure/Launch.vue
@@ -5,10 +5,13 @@
             <span class="text-muted flex-grow-1">
                 If you launch the poll, you will not be able to edit it again.
             </span>
-            <b-button class="button-spacing">
+            <b-button class="button-spacing"
+                      @click="launchAndScheduleNew">
                 Schedule
             </b-button>
-            <b-button variant="primary" class="button-spacing">
+            <b-button variant="primary"
+                      class="button-spacing"
+                      @click="launchAndOpenNew">
                 Launch now!
             </b-button>
         </div>
@@ -16,8 +19,37 @@
 </template>
 
 <script>
+    import {mapActions} from "vuex"
+
     export default {
-        name: "Launch.vue"
+        name: "Launch.vue",
+        methods: {
+            ...mapActions("currentPoll", {
+                launch: "launch"
+            }),
+            ...mapActions("currentPoll/iterations", {
+                scheduleNew: "scheduleNew",
+                openNew: "openNew"
+            }),
+
+            async launchAndScheduleNew() {
+                try {
+                    await this.launch();
+                    this.scheduleNew();
+                } catch(err) {
+                    console.warn(err);
+                }
+            },
+
+            async launchAndOpenNew() {
+                try {
+                    await this.launch();
+                    this.openNew();
+                } catch(err) {
+                    console.warn(err);
+                }
+            }
+        }
     }
 </script>
 
