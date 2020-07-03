0531caf2ac5783941a25738568c06ada856f7b2c
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-07-03 17:03:13 +0200

    Poll Config Elements Launch and Iterations are now shown / hidden according to poll status. refs #15704 @0.1h also removed old SelectStatus.vue

--- src/main/vue/components/poll-tabs/configure/ConfigurePoll.vue
+++ src/main/vue/components/poll-tabs/configure/ConfigurePoll.vue
@@ -4,20 +4,18 @@
             <Anonymity></Anonymity>
         </p>
 
-        <p>
+        <p v-if="poll.status === 'EDITING'">
+            <Launch></Launch>
+        </p>
+
+        <p v-else-if="poll.status === 'LAUNCHED'">
             <Iterations/>
         </p>
 
         <p>
-            <Launch></Launch>
-        </p>
-
-        <p>
-            <SelectStatus></SelectStatus>
-        </p>
-        <p>
             <Design></Design>
         </p>
+
         <p>
             <ManageParticipants></ManageParticipants>
         </p>
@@ -33,7 +31,6 @@
 </template>
 
 <script>
-    import SelectStatus from "./SelectStatus";
     import ManageParticipants from "./ManageParticipants";
     import DeletePoll from "./DeletePoll";
     import Anonymity from "./Anonymity";
@@ -42,11 +39,23 @@
     import Launch from "./Launch";
     import Iterations from "./Iterations";
 
+    import {mapState} from "vuex"
+
     export default {
         name: "ConfigurePoll",
         components: {
             Iterations,
-            Launch, Download, Design, Anonymity, SelectStatus, ManageParticipants, DeletePoll
+            Launch,
+            Download,
+            Design,
+            Anonymity,
+            ManageParticipants,
+            DeletePoll
+        },
+        computed: {
+            ...mapState("currentPoll", {
+                poll: "poll"
+            })
         }
     }
 
