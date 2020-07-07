dca2fd7a110d3bacfbf19ac29bd2862a21ca795d
lkrueger <lkrueger@techfak.uni-bielefeld.de>
2020-07-07 17:11:36 +0200

    Renaming a poll to "" now sets the title to "Unnamed Poll" refs #16214

--- src/main/vue/pages/PollTabbed.vue
+++ src/main/vue/pages/PollTabbed.vue
@@ -87,6 +87,9 @@
             setEditingTitle(edit) {
                 this.editTitle = edit;
                 if (!edit) {
+                    if (this.pollTitle === "") {
+                        this.pollTitle = "Unnamed Poll"
+                    }
                     this.update({id: this.poll.id, title: this.pollTitle});
                 }
             }
