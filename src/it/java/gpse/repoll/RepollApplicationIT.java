bd2a5dbd518ed62e79f4a883ae0100b94183de4e
Lukas Middelberg <lmiddelberg@techfak.uni-bielefeld.de>
2020-07-09 15:06:11 +0200

    Disbled productionmode in RepollApplicationIT #16042 @0.01h

--- src/it/java/gpse/repoll/RepollApplicationIT.java
+++ src/it/java/gpse/repoll/RepollApplicationIT.java
@@ -3,10 +3,12 @@ package gpse.repoll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = RepollApplication.class)
+@TestPropertySource(properties = { "repoll.productionMode=false" })
 public class RepollApplicationIT {
 
     @Test
